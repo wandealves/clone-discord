c12185ecd65ef833f887f8a7391f9d0eb3efd210
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2021-10-31 15:23:06 -0300

    feat: criação do componente server name

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import ServerList from '../ServerList'
+import ServerName from '../ServerName'
 
 import {Grid} from './styles'
 
 const Layout = () =>{
 return (<Grid>
   <ServerList/>
+  <ServerName/>
 </Grid>)
 }
 
---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import {Container, Title, ExpandIcon} from './styles'
+
+const ServerName = () =>{
+return (<Container>
+  <Title>Servidor do Rodz</Title>
+  <ExpandIcon/>
+</Container>)
+}
+
+export default ServerName
